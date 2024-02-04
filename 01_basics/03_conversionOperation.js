00df1278eade162f5e28abd08dc34e24049a0e2b
jhawarnikhlesh <158809052+jhawarnikhlesh@users.noreply.github.com>
2024-02-04 11:24:26 +0000

    convertion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,47 @@
+// Case 1
+
+// let score="30";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber =Number(score);
+// console.log(typeof(valueInNumber));
+
+
+// Case 2
+// let scoreInString = "30abl";
+// let valueInNumberinstrin = Number(scoreInString);
+// let valueInNumberinstring = String(scoreInString);
+// console.log(typeof (valueInNumberinstrin));
+// console.log(valueInNumberinstrin);
+
+// console.log(typeof (valueInNumberinstring));
+// console.log(valueInNumberinstring);
+
+// // Case 3
+// let scoreInString = false;
+// let valueInNumberinstring = Number(scoreInString);
+// console.log(typeof (valueInNumberinstring));
+// console.log(valueInNumberinstring);
+
+// // Case 4
+// let scoreInString = true;
+// let valueInNumberinstring = Number(scoreInString);
+// console.log(typeof (valueInNumberinstring));
+// console.log(valueInNumberinstring);
+
+
+// // Case 5
+// let scoreInString = null;
+// let valueInNumberinstring = Number(scoreInString);
+// console.log(typeof (valueInNumberinstring));
+// console.log(valueInNumberinstring);
+
+// Case 5
+let scoreInString = undefined;
+let valueInNumberinstring = Number(scoreInString);
+console.log(typeof (valueInNumberinstring));
+console.log(valueInNumberinstring);
+
+
